09501d22fd2d7f5b71dcff9046d8c048d83bac21
Joseph Poirier <jdpoirier@gmail.com>
2015-06-14 15:58:46 -0500

    Partial fix (e.g. if (somepointer) but not if (!somepointer)) for issue #5

--- expression.c
+++ expression.c
@@ -1907,7 +1907,8 @@ long ExpressionParseInt(struct ParseState *Parser)
         ProgramFail(Parser, "expression expected");
 
     if (Parser->Mode == RunModeRun) {
-        if (!IS_NUMERIC_COERCIBLE(Val))
+        // if (!IS_NUMERIC_COERCIBLE(Val))
+        if (!IS_NUMERIC_COERCIBLE_PLUS_POINTERS(Val, true))
             ProgramFail(Parser, "integer value expected instead of %t", Val->Typ);
 
         Result = ExpressionCoerceInteger(Val);
